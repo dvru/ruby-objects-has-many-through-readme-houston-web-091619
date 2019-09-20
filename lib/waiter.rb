56ad68236f7ae122494b3c874516f935188910fd
Damini Varu <damvaru4@gmail.com>
2019-09-20 22:42:42 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -28,7 +28,7 @@ attr_accessor :name, :years
   
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
-      meal_a.top <=> meal_b.tip
+      meal_a.tip <=> meal_b.tip
   end 
   
   best_tipped_meal.customer
