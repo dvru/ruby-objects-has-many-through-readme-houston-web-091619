4667da3e2f5784f3d31a57d48c7286ace71b9a44
Damini Varu <damvaru4@gmail.com>
2019-09-20 22:31:10 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
 
+attr_accessor :name, :years 
+
+@@all = []
+
+
+  def initialize(name, years)
+    @name = name 
+    @year = years
+    @@all << self 
+    
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_meal(waiter, total, tip)
+     Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals 
+    Meal.all.select do |meal|
+      meal.customer == self
+    end 
+  end 
+  
+  
+  
 end
